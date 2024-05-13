c78913d77ca8ae8f3bc9a83e30f1379cb7c75768
kawtar24 <k.mounir@mundiapolis.ma>
2024-05-13 14:11:58 +0100

    test

--- perfect/urls.py
+++ perfect/urls.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from django.urls import path, include 
 
 import authentication
-#import centers.urls
-#import courses
-#import payments
+import centers.urls
+import courses
+import payments
 
 urlpatterns = [
     path('users/', include('authentication.urls')),
-    #path('centers/', include('centers.urls')),
+    path('centers/', include('centers.urls')),
     path('courses/', include('courses.urls')),
-    #path('payments/', include('payments.urls')),
+    path('payments/', include('payments.urls')),
 ]
